4e0d51f42557da76058c29aca632c744d18f11a5
shresthaRahul <rahul.shrestha@selu.edu>
2015-02-18 01:16:38 -0600

    Forgot to do it in the previous one! Only 1 change! Added a method to see if the email addresses are unique.

--- Phase2_Group2_selucmps383_sp15_p2_g2/Models/User.cs
+++ Phase2_Group2_selucmps383_sp15_p2_g2/Models/User.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using FluentValidation;
+using Phase2_Group2_selucmps383_sp15_p2_g2.DbContext;
 
 namespace Phase2_Group2_selucmps383_sp15_p2_g2.Models
 {
+    [FluentValidation.Attributes.Validator(typeof(PlaceValidator))]
     public class User
     {
         public int UserId { get; set; }
@@ -28,5 +31,24 @@ namespace Phase2_Group2_selucmps383_sp15_p2_g2.Models
         public List<Game> Games { get; set; }
         public List<Sale> Sales { get; set; } 
 
+    }
+
+
+    /**
+     * Fluent Validation to make sure that the Email-Addresses are unique
+     */
+    public class PlaceValidator : AbstractValidator<User>
+    {
+        public PlaceValidator()
+        {
+            RuleFor(x => x.EmailAddress).Must(BeUnique).WithMessage("UserName Already Exists! Try Again!!!");
+        }
+
+        private bool BeUnique(string username)
+        {
+            var _db = new GameStoreContext();
+            if (_db.Users.SingleOrDefault(x => x.EmailAddress == username) == null) return true;
+            return false;
+        }
     }
 }
