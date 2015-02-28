9de74e5cf1e183724d57b09dd091467a45933467
shresthaRahul <rahul.shrestha@selu.edu>
2015-02-28 14:51:58 -0600

    created NinjectWebCommon and configured it to have a single instance of database context object shared by all objects created via the kernel for that HTTP request. {{{{{{{Reason to create it: when we created the GameStoreRepository, its constructor accepts the database context "GameStoreContext" so, GSR depends on the database context to work, we need the Register in Ninject Kernel. }}}}}}}

---
+++ Phase2_Group2_selucmps383_sp15_p2_g2/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Ninject;
+using Ninject.Web.Common;
+using Phase2_Group2_selucmps383_sp15_p2_g2.DbContext;
+using Phase2_Group2_selucmps383_sp15_p2_g2.Models;
+
+namespace Phase2_Group2_selucmps383_sp15_p2_g2.App_Start
+{
+   
+ public static class NinjectWebCommon
+    {
+ 
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+ 
+            //Suport WebAPI Injection
+            GlobalConfiguration.Configuration.DependencyResolver = new WebApiContrib.IoC.Ninject.NinjectResolver(kernel);
+ 
+            RegisterServices(kernel);
+            return kernel;
+        }
+ 
+     /// <summary>
+     /// Load the modules or register services here
+     /// </summary>
+     /// <param name="kernel"></param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<GameStoreContext>().To<GameStoreContext>().InRequestScope();
+            kernel.Bind<IGameStoreRepository>().To<IGameStoreRepository>().InRequestScope();
+        }
+    }
+}
