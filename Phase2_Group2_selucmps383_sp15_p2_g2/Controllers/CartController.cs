2f66474bd148e6e7cf9de0e6201721508b1377c9
kburkhalter <kyle.burkhalter@selu.edu>
2015-02-28 15:49:11 -0600

    added CartController

---
+++ Phase2_Group2_selucmps383_sp15_p2_g2/Controllers/CartController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Phase2_Group2_selucmps383_sp15_p2_g2.Models;
+using Phase2_Group2_selucmps383_sp15_p2_g2.DbContext;
+
+namespace Phase2_Group2_selucmps383_sp15_p2_g2.Controllers
+{
+    public class CartController : ApiController
+    {
+        private GameStoreContext db = new GameStoreContext();
+
+        // GET api/Cart
+        public IQueryable<Cart> GetCarts()
+        {
+            return db.Carts;
+        }
+
+        // GET api/Cart/5
+        [ResponseType(typeof(Cart))]
+        public async Task<IHttpActionResult> GetCart(int id)
+        {
+            Cart cart = await db.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cart);
+        }
+
+        // PUT api/Cart/5
+        public async Task<IHttpActionResult> PutCart(int id, Cart cart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cart.CartId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cart).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Cart
+        [ResponseType(typeof(Cart))]
+        public async Task<IHttpActionResult> PostCart(Cart cart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Carts.Add(cart);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = cart.CartId }, cart);
+        }
+
+        // DELETE api/Cart/5
+        [ResponseType(typeof(Cart))]
+        public async Task<IHttpActionResult> DeleteCart(int id)
+        {
+            Cart cart = await db.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            db.Carts.Remove(cart);
+            await db.SaveChangesAsync();
+
+            return Ok(cart);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CartExists(int id)
+        {
+            return db.Carts.Count(e => e.CartId == id) > 0;
+        }
+    }
+}
