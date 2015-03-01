139dae8deb4d37802a4dc38e6547e92fb29694ee
shresthaRahul <rahul.shrestha@selu.edu>
2015-02-28 22:47:48 -0600

    role based authentication for data access through Role enum in BaseApiController.

--- Phase2_Group2_selucmps383_sp15_p2_g2/Controllers/BaseApiController.cs
+++ Phase2_Group2_selucmps383_sp15_p2_g2/Controllers/BaseApiController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Phase2_Group2_selucmps383_sp15_p2_g2.Enums;
 using Phase2_Group2_selucmps383_sp15_p2_g2.Models;
 
 namespace Phase2_Group2_selucmps383_sp15_p2_g2.Controllers
 {
     public class BaseApiController : ApiController
     {
+        private User storeUser;
         private IGameStoreRepository _repo ;
 
         public BaseApiController(IGameStoreRepository repo)
         {
             _repo = repo;
+            storeUser = new User();
         }
 
         protected IGameStoreRepository TheRepository
@@ -22,5 +25,24 @@ namespace Phase2_Group2_selucmps383_sp15_p2_g2.Controllers
             get { return _repo; }
         }
 
+        protected bool IsStoreAdmin()
+        {
+            return Enum.GetName(typeof(Role), storeUser.Role)==("StoreAdmin");
+        }
+
+        protected bool IsEmployee()
+        {
+            if (IsStoreAdmin())
+            {
+                return true;
+            }
+
+            return Enum.GetName(typeof (Role), storeUser.Role) == ("StoreEmployee");
+        }
+
+        protected bool IsCustomer()
+        {
+            return Enum.GetName(typeof (Role), storeUser.Role) == ("StoreCustomer");
+        }
     }
 }
