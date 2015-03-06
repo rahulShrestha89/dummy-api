3ca03b5b32d0a367cecd92fcce09de6d3ca54316
shresthaRahul <rahul.shrestha@selu.edu>
2015-03-06 01:44:17 -0600

    abstract base controller derived from api controller. commented for easier understanding!

--- Phase2_Group2_selucmps383_sp15_p2_g2/Controllers/BaseApiController.cs
+++ Phase2_Group2_selucmps383_sp15_p2_g2/Controllers/BaseApiController.cs
@@ -10,12 +10,17 @@ using Phase2_Group2_selucmps383_sp15_p2_g2.DbContext;
 
 namespace Phase2_Group2_selucmps383_sp15_p2_g2.Controllers
 {
-    public class BaseApiController : ApiController
+    // make base controller abstract so it cannot be instantiated
+    // all our controllers will derive from the base controller to
+    // inherit common functionality
+    public abstract class BaseApiController : ApiController
     {
         public User storeUser;
+        public GameStoreContext _db;
+        
         public IGameStoreRepository _repo ;
         public ModelFactory _modelFactory;
-        public GameStoreContext _db;
+        
 
         public BaseApiController(IGameStoreRepository repo)
         {
@@ -28,12 +33,24 @@ namespace Phase2_Group2_selucmps383_sp15_p2_g2.Controllers
             get { return _repo; }
         }
 
+
+        // key point: we need to defer the creation of the model factory until
+        // it is needed. So here we create a deferred property.
+        // This is basically a singleton pattern, which will exist for the lifetime of the controller
         protected ModelFactory TheModelFactory
         {
+
+            // remember we're using the factory pattern to copy database entities into our models that contain discoverable values for each resources
             get
             {
+                // the first get will cause the factory to be created
                 if (_modelFactory == null)
                 {
+                    // it should be late enough for the request to not be null
+                    // the model factory will be created as a result of the user request
+                    // so we can create the model factory and pass it the request so we may
+                    // use it and the Url helper to generate the requested resource's URI hyperlinks for including
+                    // in the models that we pass back to the user
                     _modelFactory = new ModelFactory(Request, TheRepository);
                 }
                 return _modelFactory;
